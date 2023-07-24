ac84beaa32618377539d1af22994f7fbd232e5e8
solomonbestz <kingsolodawarrior@gmail.com>
2023-07-24 16:35:54 +0100

    updating the win function

--- scripts.js
+++ scripts.js
@@ -15,33 +15,33 @@ function create_elements(element_name,class_name, text, parant_name){
 // 6, 7, 8
 
 function check_win(arr){
-    const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]]
+    const wins = { 0: [0, 1, 2], 1: [3, 4, 5], 2: [6, 7, 8]}
     let count = 0
-    for(let i = 0; i < wins.length; i++){
-        for(let j = 0; j < arr.length; j++){
-            if(arr[j] == wins[i][j]){
+    for(let j = 0; j <= 2; j++){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] in wins[`${j}`]){
                 count += 1
                 console.log(`Count is ${count}`)
-            }
-            else{
-                count = 0
             }
         }
         if(count == 3){
             break
         }
+        count = 0
+        console.log(`Iterated ${j+1}`)
     }
     return count
 }
 
 function anoounce_win(){
-    if(x_play.length == 3){
+    if(x_play.length >= 3){
         if(check_win(x_play) == 3){
-            alert('X Wins')
+            win_board.innerText = 'X WINS'
         }
-    }else if(o_play.length == 3){
-        if(check_win(x_play) == 3){
-            alert('O Wins')
+    }
+    if(o_play.length >= 3){
+        if(check_win(o_play) == 3){
+            win_board.innerText = 'O WINS'
         }
     }
 }
@@ -55,14 +55,16 @@ const o_play = []
 
 // Creating the main div container
 let main_container = create_elements('div', 'container', '', document.body)
+let announcement_cont = create_elements('div', 'announce', '', main_container)
 let board_container = create_elements('div', 'board', '', main_container)
+let win_board = create_elements('p', 'winner', '', announcement_cont)
 
 for(let i = 0; i < 9; i++){
     const button  = create_elements('button', 'btn', '', board_container)
     btn_object.push(button)
 }
-    
 
+// Looping through each button object added as an item in btn_object list
 btn_object.forEach(function callback(btn, index){
     btn.addEventListener('click', function (){
         btn.innerText = choice
@@ -79,6 +81,7 @@ btn_object.forEach(function callback(btn, index){
             first_play = true
             second_play = false
             o_play.push(index)
+            anoounce_win()
             console.log(o_play)
         }
     })
